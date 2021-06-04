7b5178d930ac9c0d3835ead6e7b2f85d162b54b8
Julian <angela.julian@gmail.com>
2021-06-03 21:31:16 -0700

    added questions and added js reference to index.html

---
+++ assets/script.js
@@ -0,0 +1,76 @@
+var questions = [
+  {
+    text: "How are Java and Javascript related?",
+    answers: [
+      "Java is named after Javascript",
+      "They are the same thing",
+      "Javascript was named after Java",
+      "Javascript is build off of Java",
+    ],
+    key: [2],
+  },
+  {
+    text: "What is true about console.log?",
+    answers: [
+      "anything printed using console.log shows up in the developer console",
+      "console.log is useful to developers to display what variables are set as at different points in an application",
+      "Console.log isn't visible on the page",
+      "all of the above",
+    ],
+    key: [3],
+  },
+  {
+    text: "Javascript is an object oriented programming language",
+    answers: [True, False],
+    key: [0],
+  },
+  {
+    text: "Javascript is an obligatory part of web design",
+    answers: ["True!", "False!"],
+    key: [1],
+  },
+  {
+    text: "Which is not a native Javascript data type?",
+    answers: ["a Boolean", "a String", "a Dictionary", "a Float"],
+    key: [2],
+  },
+  {
+    text: "Strings must be enclosed in ____",
+    answers: [
+      "Double or single quotes",
+      "Square Brackets",
+      "Double quotes, only",
+      "Single quotes, only",
+    ],
+    key: [true, false, false, false],
+  },
+  {
+    text: "4.5 is an example of",
+    answers: ["a char", "a float", "an int", "an array"],
+    key: [1],
+  },
+  {
+    text: "When in a function, 'return x' does what?",
+    answers: [
+      "breaks out of a function",
+      "passes 'x' outside the function",
+      "both of the above",
+      "prints 'x' to the console",
+    ],
+    key: [2],
+  },
+  {
+    text: "How would you change the title of the following object? \n var customer = { title: 'miss', name: 'E. Elliot'}",
+    answers: [
+      "customer = 'Mrs', 'E. Eliot'",
+      "customer.name = 'Mr'",
+      "name = 'Mz'",
+    ],
+    key: [1],
+  },
+  {
+    text: "Bonus question: Who wrote this quiz?",
+    answers: [" Julianne ", " Julie ", " Adrian ", " Angela "],
+    key: [3],
+  },
+];
